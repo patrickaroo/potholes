8f072fd551cd66c0a29631166e13a076393f5578
Patrick <patrickaroo@gmail.com>
2013-04-28 22:11:30 -0500

    initial setup and commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , search = require('./routes/search')
+  , http = require('http')
+  , path = require('path')
+  , mongoose = require('mongoose');
+
+var app = express();
+mongoose.connect('localhost', 'potholedata');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+app.get('/findPotholesByLoc', search.byLoc);
+// app.get('/findPotholesByAddress', search.byAddress);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ dbinit.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# mongoimport --db potholesdata --collection potholes --type csv --headerline --file osd-bike-racks/data/Bike_Racks.csv
+
+mongoimport --db potholesdata --collection potholes --type csv --headerline --file data/311_Service_Requests_-_Pot_Holes_Reported.csv
+
+# mongo dataclean.js

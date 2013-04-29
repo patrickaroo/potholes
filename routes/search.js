e323b22ea4096798b5ddd8f0623dabf6cdeae079
Patrick <patrickaroo@gmail.com>
2013-04-28 23:58:36 -0500

    improve routes/search.js

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 var PotholeSchema = new mongoose.Schema({
-        Address: String,
-        Latitude: String,
-        Longitude: String
+        ADDRESS: String,
+        LATITUDE: String,
+        LONGITUDE: String
     },
     {
         collection: 'potholes'
@@ -16,9 +16,9 @@ exports.byLoc = function(req, res){
     console.log(req.query.lat, req.query.lng);
     return Pothole.find({
             loc: { $nearSphere:[ req.query.lng, req.query.lat ], $maxDistance: 100}
-            }).limit(5).execFind(function(err, rack){
-            console.log('here', rack);
-            res.send(rack);
+            }).limit(5).execFind(function(err, pothole){
+            console.log('here', pothole);
+            res.send(pothole);
     });
 };
 
